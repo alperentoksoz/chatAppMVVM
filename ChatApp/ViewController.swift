40a3bf0adadbed9b89699235d677bda34b4c215e
aetoksoz <aetoksoz@gmail.com>
2020-05-25 13:14:17 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Alperen Toksöz on 25.05.2020.
+//  Copyright © 2020 Alperen Toksöz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
